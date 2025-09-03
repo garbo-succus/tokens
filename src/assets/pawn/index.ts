4321fe96dcb1f2aa6c88aad75f60a9dba063acf7
Egg Carton Games <joe.rickwood@mds.ac.nz>
2025-09-03 21:19:54 +1200

    Add Pawn
    Normal maps are slightly wrong, there are artifacts, may need re-baking but I would like a second opinion

---
+++ src/assets/pawn/index.ts
@@ -0,0 +1,10 @@
+import type { PieceTemplate } from "../../types";
+
+const src = "./pawn/pawn.gltf";
+
+export const pawn = {
+  // Asset originally created by Misha Tsyatksko for Garbo Succus
+  name: "pawn",
+  src,
+  scale: [1.0, 1.0, 1.0],
+} as PieceTemplate;
