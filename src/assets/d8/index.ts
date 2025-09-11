7deabde254ddf414704d7f9ef458d6ec234e2255
Egg Carton Games <98846669+JoeRickwood@users.noreply.github.com>
2025-09-11 23:57:50 +1200

    Add D8 (#8)

---
+++ src/assets/d8/index.ts
@@ -0,0 +1,20 @@
+import type { PieceTemplate } from "../../types";
+
+const src = "./d8/d8.gltf";
+
+export const d8 = {
+  // Asset originally created by Misha Tsyatksko for Garbo Succus
+  name: "D8 Dice",
+  src,
+  scale: [0.016, 0.016, 0.016], //Scaled to fit inside a 16mm^3 cube
+  faces: [
+    { name: "1", rotation: [-55, 0, 0] },
+    { name: "2", rotation: [125, 0, 0] },
+    { name: "3", rotation: [-55, 90, 180] },
+    { name: "4", rotation: [125, 90, 180] },
+    { name: "5", rotation: [125, 0, 180] },
+    { name: "6", rotation: [-125, 0, 0] },
+    { name: "7", rotation: [-125, 90, 0] },
+    { name: "8", rotation: [-125, 90, 180] },
+  ],
+} as PieceTemplate;
